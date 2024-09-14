3cba0cfd075afb7036ab023c9d2ad752618cd79c
GithubCrafter <ben.sc.schlueter@gmail.com>
2024-09-15 01:11:22 +0200

    Delete Instant Respawn.lua

--- Instant Respawn.lua
+++
@@ -1,9 +0,0 @@
-game:GetService("Players").PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(character)
-        character:WaitForChild("Humanoid").Died:Connect(function()
-            player:LoadCharacter()
-        end)
-    end)
-end)
-
--- put this in a script in ServerScriptService
